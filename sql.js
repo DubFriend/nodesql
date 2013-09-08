e404beb0e1a71dae01da744009b68e8971f82643
Brian Detering <bdeterin@gmail.com>
2013-09-07 21:58:55 -0700

    refactoring and implemented helper methods

--- sql.js
+++ sql.js
@@ -9,43 +9,99 @@ var getCallback = function (args) {
     return (_.isFunction(args[1]) ? args[1] : args[2]) || function () {};
 };
 
+//returns an array of given length,
+//with all values initialized to the given value
+var pad = function (length, value) {
+    var i, array = [];
+    for(i = 0; i < length; i += 1) {
+        array[i] = value;
+    }
+    return array;
+};
+
+
+var baseStrategy = (function () {
+    var equalsToSql = function (whereEqualsKeys) {
+            return _.map(whereEqualsKeys, function (key) {
+                return key + ' = ?';
+            }).join(', ');
+        },
+        select = function (context, table, whereEquals, callback) {
+            context.query(
+                "SELECT * FROM " + table + " WHERE " + equalsToSql(_.keys(whereEquals)),
+                _.values(whereEquals),
+                callback
+            );
+        };
+
+    return {
+        one: function (statement, a, b) {
+            var callback = getCallback(arguments);
+            a = _.isFunction(a) ? [] : a;
+            this.query(statement, a, function (err, rows) {
+                callback(err, rows ? rows[0] : []);
+            });
+        },
+        select: function (table, whereEquals, callback) {
+            select(this, table, whereEquals, callback);
+        },
+        selectOne: function (table, whereEquals, callback) {
+            select(this, table, whereEquals, function (err, rows) {
+                callback(err, rows ? rows[0] : []);
+            });
+        },
+        insert: function (table, values, callback) {
+            this.query(
+                'INSERT INTO ' + table + '(' + _.keys(values).join(', ') + ') ' +
+                'VALUES (' + pad(_.values(values).length, '?').join(', ') + ')',
+                _.values(values),
+                callback
+            );
+        },
+        update: function (table, values, whereEquals, callback) {
+            this.query(
+                'UPDATE ' + table + ' SET ' + equalsToSql(_.keys(values)) + ' ' +
+                'WHERE ' + equalsToSql(_.keys(whereEquals)),
+                _.values(values).concat(_.values(whereEquals)),
+                callback
+            );
+        },
+        delete: function (table, whereEquals, callback) {
+            this.query(
+                'DELETE FROM ' + table + ' WHERE ' + equalsToSql(_.keys(whereEquals)),
+                _.values(whereEquals),
+                callback
+            );
+        }
+    };
+}());
+
 exports.createMySqlStrategy = function (connection) {
     'use strict';
-    var that = {};
+    var that = Object.create(baseStrategy);
 
     that.query = function (statement, a, b) {
-        var callback = getCallback(arguments);
-        a = _.isFunction(a) ? null : a;
+
+        var callback = getCallback(arguments),
+            query = _.bind(connection.query, connection, statement, a),
+            defaultQuery = _.partial(query, callback);
+
+        a = _.isFunction(a) ? [] : a;
+
         switch(extractQueryType(statement)) {
             case 'SELECT':
-                connection.query(statement, a, function (err, rows) {
-                    if(err) {
-                        callback(err);
-                    }
-                    else {
-                        callback(null, rows);
-                    }
-                });
+                defaultQuery();
                 break;
             case 'INSERT':
-                connection.query(statement, a, function (err, response) {
-                    if(err) {
-                        callback(err);
-                    }
-                    else {
-                        callback(null, response.insertId);
-                    }
+                query(function (err, response) {
+                    callback(err, err ? undefined : response.insertId);
                 });
                 break;
             case 'UPDATE':
-                connection.query(statement, a, function (err) {
-                    callback(err ? false : true);
-                });
+                defaultQuery();
                 break;
             case 'DELETE':
-                connection.query(statement, a, function (err) {
-                    callback(err ? false : true);
-                });
+                defaultQuery();
                 break;
             default:
                 throw 'Invalid Query Type';
@@ -55,42 +111,32 @@ exports.createMySqlStrategy = function (connection) {
     return that;
 };
 
+
 exports.createSqliteStrategy = function (connection) {
     'use strict';
-    var that = {};
+    var that = Object.create(baseStrategy);
 
     that.query = function (statement, a, b) {
-        var callback = getCallback(arguments);
+        var callback = getCallback(arguments),
+            query = _.bind(connection.run, connection, statement, a),
+            defaultQuery = _.partial(query, callback);
+
+        a = _.isFunction(a) ? [] : a;
+
         switch(extractQueryType(statement)) {
             case 'SELECT':
-                connection.all(statement, a, function (err, rows) {
-                    if(err) {
-                        callback(err);
-                    }
-                    else {
-                        callback(null, rows);
-                    }
-                });
+                connection.all(statement, a, callback);
                 break;
             case 'INSERT':
-                connection.run(statement, a, function (err) {
-                    if(err) {
-                        callback(err);
-                    }
-                    else {
-                        callback(null, this.lastID);
-                    }
+                query(function (err) {
+                    callback(err, this.lastID);
                 });
                 break;
             case 'UPDATE':
-                connection.run(statement, a, function (err) {
-                    callback(err ? false : true);
-                });
+                defaultQuery();
                 break;
             case 'DELETE':
-                connection.run(statement, a, function (err) {
-                    callback(err ? false : true);
-                });
+                defaultQuery();
                 break;
             default:
                 throw 'Invalid Query Type';
--- sqlTest.js
+++ sqlTest.js
@@ -13,17 +13,19 @@ var sql = require('./sql'),
         database: 'test'
     }),
 
-    createMysqlDatabase = '' +
+    createMysqlDatabaseStatement = '' +
     'CREATE TABLE TableA (' +
         'id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, ' +
         'col VARCHAR(64)' +
     ')',
 
-    createSqliteDatabase = '' +
+    createSqliteDatabaseStatement = '' +
     'CREATE TABLE TableA (' +
         'id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
         'col VARCHAR(64)' +
-    ')';
+    ')',
+
+    defaultRowStatement = 'INSERT INTO TableA (id, col) VALUES (2, "default")';
 
 var createTests = function (fig) {
     'use strict';
@@ -54,7 +56,7 @@ var createTests = function (fig) {
     };
 
     //depends on testQuerySelect
-    that.testInsert = function (test) {
+    that.testQueryInsert = function (test) {
         test.expect(2);
         var that = this;
         that.sql.query(
@@ -76,13 +78,13 @@ var createTests = function (fig) {
     };
 
     //depends on testQuerySelect
-    that.testUpdate = function (test) {
+    that.testQueryUpdate = function (test) {
         test.expect(2);
         var that = this;
         that.sql.query(
             'UPDATE TableA SET col="edit" WHERE id = 2',
             function (isSuccess) {
-                test.strictEqual(isSuccess, true, 'callback param set to true');
+                test.strictEqual(isSuccess, null, 'callback param set to true');
                 that.sql.query('SELECT * FROM TableA', function (err, rows) {
                     test.deepEqual(
                         rows, [{ id: 2, col: 'edit' }], 'row is updated'
@@ -94,19 +96,82 @@ var createTests = function (fig) {
     };
 
     //depends on testQuerySelect
-    that.testDelete = function (test) {
+    that.testQueryDelete = function (test) {
         test.expect(2);
         var that = this;
         that.sql.query(
             'DELETE FROM TableA WHERE id = 2',
             function (isSuccess) {
-                test.strictEqual(isSuccess, true, 'callback param set to true');
+                test.strictEqual(isSuccess, null, 'callback param set to true');
                 that.sql.query('SELECT * FROM TableA', function (err, rows) {
                     test.deepEqual(rows, [], 'row is deleted');
                     test.done();
                 });
             }
         );
+    };
+
+    that.testOne = function (test) {
+        test.expect(1);
+        this.sql.one('SELECT col FROM TableA', function (err, rows) {
+            test.deepEqual(rows, { col: 'default' });
+            test.done();
+        });
+    };
+
+    that.testSelect = function (test) {
+        test.expect(1);
+        this.sql.select('TableA', { id: 2 }, function (err, rows) {
+            test.deepEqual(rows, [{ id: 2, col: "default"}]);
+            test.done();
+        });
+    };
+
+    that.testSelectOne = function (test) {
+        test.expect(1);
+        this.sql.selectOne('TableA', { id: 2 }, function (err, row) {
+            test.deepEqual(row, { id: 2, col: "default"});
+            test.done();
+        });
+    };
+
+    //depends on testQuerySelect
+    that.testInsert = function (test) {
+        test.expect(2);
+        var that = this;
+        that.sql.insert('TableA', { id: 7, col: 'foo' }, function (err, insertId) {
+            test.strictEqual(insertId, 7, 'passes callback insert id');
+            that.sql.query('SELECT * FROM TableA WHERE id = 7', function (err, rows) {
+                test.deepEqual(rows, [{ id: 7, col: 'foo' }], 'row is inserted');
+                test.done();
+            });
+        });
+    };
+
+    //depends on testQuerySelect
+    that.testUpdate = function (test) {
+        test.expect(2);
+        var that = this;
+        that.sql.update('TableA', { col: 'edit' }, { id: 2 }, function (err) {
+            test.strictEqual(err, null, 'error not set');
+            that.sql.query('SELECT * FROM TableA WHERE id = 2', function (err, rows) {
+                test.deepEqual(rows, [{ id: 2, col: 'edit' }], 'row is edited');
+                test.done();
+            });
+        });
+    };
+
+    //depends on testQuerySelect
+    that.testDelete = function (test) {
+        test.expect(2);
+        var that = this;
+        that.sql.delete('TableA', { id: 2 }, function (err) {
+            test.strictEqual(err, null, 'error not set');
+            that.sql.query('SELECT * FROM TableA WHERE id = 2', function (err, rows) {
+                test.deepEqual(rows, [], 'row is deleted');
+                test.done();
+            });
+        });
     };
 
     return that;
@@ -116,8 +181,8 @@ var createTests = function (fig) {
 exports.mysql = createTests({
     setUp: function () {
         mysqlConnection.query('DROP TABLE TableA');
-        mysqlConnection.query(createMysqlDatabase);
-        mysqlConnection.query('INSERT INTO TableA (id, col) VALUES (2, "default")');
+        mysqlConnection.query(createMysqlDatabaseStatement);
+        mysqlConnection.query(defaultRowStatement);
         return sql.createMySqlStrategy(mysqlConnection);
     }
 });
@@ -126,16 +191,16 @@ exports.sqlite3 = createTests({
     setUp: function () {
         sqliteConnection = new sqlite.Database(':memory:');
         sqliteConnection.serialize(function () {
-            sqliteConnection.run(createSqliteDatabase);
-            sqliteConnection.run('INSERT INTO TableA (id, col) VALUES (2, "default")');
+            sqliteConnection.run(createSqliteDatabaseStatement);
+            sqliteConnection.run(defaultRowStatement);
         });
         return sql.createSqliteStrategy(sqliteConnection);
     }
 });
 
-
-//very gross solution, mysql can only be disconnected once (and test runner wont
+//TODO
+//very gross solution. mysql can only be disconnected once (and test runner wont
 //exit untill connection is closed)
 setTimeout(function () {
     mysqlConnection.end();
-}, 3000);
+}, 6000);
